9c96c39487301c2370bf78cb6e158b3d33482251
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-16 14:39:00 -0700

    added components inside src folder

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,8 +1,26 @@
-import react from react;
+import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap-nav';
-import './Nav.css';
+import { Link } from 'react-router-dom';
+import './navbar.css';
 
+function Navbar(){
+    return (
+        <div className='navbar'>
+           <div className='toggleButton'>
+               <button></button>
+           </div>
+           <div className='links'>
+               <Link to='/'>Home</Link>
+               <Link to='/about'>About Me</Link>
+               <Link to='/portfolio'>Portfolio</Link>
+               <Link to='/resume'>Resume</Link>
+               <Link to='/contact'>Contact</Link>
+           </div>
+       </div>
 
+    )
+       
+}
 
-export default Navigation;
+export default Navbar;
