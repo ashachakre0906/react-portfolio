fe78c22810577ff8c0fcc928d643ce8038049a75
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-19 16:49:32 -0700

    created animated title for header through figma and vivus

--- src/components/About/header.js
+++ src/components/About/header.js
@@ -1,9 +1,12 @@
 import React, {useEffect} from 'react';
-
-const mainTitle = () =>{
+import Vivus from 'vivus';
+const MainTitle = () =>{
+    useEffect(() => {
+        new Vivus('main-title',{duration: 200, file:'../../../public/svg/main-title_animated.svg'})
+    },[])
     return (
-        
-
+        <div id='main-title'></div>
     )
 
 }
+export default MainTitle;
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,14 +1,15 @@
-import react from 'react';
-import image from '../../assets/images/mypic.png';
-import './about.css'
+import react from "react";
+import image from "../../assets/images/mypic.png";
+import "./about.css";
+import MainTitle from "./header";
 
 const About = () => {
-    return (
-        <div className='about-me'>
-            <img src={image} alt='Asha Chakre'/>
-            <h1 className='main-title'>Hi, I'm Asha Chakre</h1>
-        </div>
-    )
-}
+  return (
+    <div className="about-me">
+      <img src={image} alt="Asha Chakre" />
+      <MainTitle />
+    </div>   
+  );
+};
 
 export default About;
