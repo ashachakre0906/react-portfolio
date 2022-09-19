9c604b0218a857c70538e616c21963488a14fc48
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-19 00:04:38 -0700

    Fixed the nav-bar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import react from "react";
 import './App.css';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
+// import Footer from './components/Footer';
 import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div>
       {/* <Navbar /> */}
-          <Router>
+    <Router>
     <Routes>
-      <Route path='/' element = {<Navbar/>}></Route>
+      <Route path='/navbar' element = {<Navbar/>}></Route>
+      {/* <Route path='/home' element = {<Home/>}></Route> */}
+      {/* <Route path='/about' element = {<About/>}></Route>
+      <Route path='/portfolio'element = {<Portfolio/>}></Route>
+      <Route path='/contact'element = {<Contact/>}></Route>
+      <Route path='/resume' element = {<Resume/>}></Route> */}
     </Routes>
     </Router>
+    {/* <Footer /> */}
     </div>
-  
   )
   
 }
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,34 +1,53 @@
 import react from "react";
-// import Nav from 'react-bootstrap/Nav';
-// import './navbar.css';
+import { Nav, Navbar, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 function Navigation() {
   return (
-    <ul className="nav justify-content-center">
-      <li className="nav-item">
-        <Link className="nav-link active" aria-current="page" to="/">
-          Home
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/about">
-          About
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/portfolio">
-          Portfolio
-        </Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/contact">Contact</Link>
-      </li>
-      <li className="nav-item">
-        <Link className="nav-link" to="/resume">Resume</Link>
-      </li>
-    </ul>
+    <>
+      <Navbar collapseOnSelect expand='lg' bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Asha Chakre</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            </Nav>
+            <Nav>
+            <Nav.Link href="#Home">Home</Nav.Link>
+            <Nav.Link href="#About">About</Nav.Link>
+            <Nav.Link href="#Portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#Portfolio">Contact</Nav.Link>
+          </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <br />
+    </>
   );
 }
+
+
+// function Navigation() {
+//   return (
+//     <ul className="nav d-flex justify-content-center bg-color-black">
+//       <li className="nav-item">
+//         <Link className="nav-link" to="/about">
+//           About
+//         </Link>
+//       </li>
+//       <li className="nav-item">
+//         <Link className="nav-link" to="/portfolio">
+//           Portfolio
+//         </Link>
+//       </li>
+//       <li className="nav-item">
+//         <Link className="nav-link" to="/contact">Contact</Link>
+//       </li>
+//       <li className="nav-item">
+//         <Link className="nav-link" to="/resume">Resume</Link>
+//       </li>
+//     </ul>
+//   );
+// }
 
 export default Navigation;
