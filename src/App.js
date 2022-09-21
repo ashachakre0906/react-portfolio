60ece3279ed557ca872ed3580666128181487d10
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-20 17:46:30 -0700

    added code to teh component footer and updated css for footer and about

--- src/App.js
+++ src/App.js
@@ -1,30 +1,29 @@
 import react from "react";
-import './App.css';
-import Navbar from './components/Navbar';
+import "./App.css";
+import Navbar from "./components/Navbar";
 // import Footer from './components/Footer';
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import About from "./components/About";
-import Home from './components/Home';
-import Footer from './components/Footer';
+import Home from "./components/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-    <Router>
-    <Routes>
-      <Route path='/' element = {<Home/>}></Route>
-      <Route path='/about' element = {<About/>}></Route>
-      {/* <Route path='/portfolio'element = {<Portfolio/>}></Route>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          {/* <Route path='/portfolio'element = {<Portfolio/>}></Route>
       <Route path='/contact'element = {<Contact/>}></Route>
       <Route path='/resume' element = {<Resume/>}></Route> */}
-    </Routes>
-    </Router>
-    <Footer />
-    </div>
-  )
-  
+        </Routes>
+      </Router>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,21 +1,21 @@
 import react from "react";
-import { Nav, Navbar, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
-import './navbar.css';
+import { Nav, Navbar, Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import "./navbar.css";
 function Navigation() {
   return (
     <>
-      <Navbar collapseOnSelect expand='lg' bg="dark" variant="dark">
+      <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
         <Container>
           <header className="header">Asha Chakre</header>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse className="nav-link" id="responsive-navbar-nav">
-          <Nav>
-          <Nav.Link href="/home">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Nav> 
+          <Navbar.Collapse className="nav-link" id="responsive-navbar-nav">
+            <Nav>
+              <Nav.Link href="/home">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
@@ -23,29 +23,5 @@ function Navigation() {
     </>
   );
 }
-
-
-// function Navigation() {
-//   return (
-//     <ul className="nav d-flex justify-content-center bg-color-black">
-//       <li className="nav-item">
-//         <Link className="nav-link" to="/about">
-//           About
-//         </Link>
-//       </li>
-//       <li className="nav-item">
-//         <Link className="nav-link" to="/portfolio">
-//           Portfolio
-//         </Link>
-//       </li>
-//       <li className="nav-item">
-//         <Link className="nav-link" to="/contact">Contact</Link>
-//       </li>
-//       <li className="nav-item">
-//         <Link className="nav-link" to="/resume">Resume</Link>
-//       </li>
-//     </ul>
-//   );
-// }
 
 export default Navigation;
