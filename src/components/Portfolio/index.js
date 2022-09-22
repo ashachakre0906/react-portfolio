245eb4cf721da642ccdcacfdee4483b5c005ed5c
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-22 16:40:22 -0700

    updated code for portfolio page,added project description,tite githuburl and deployurl

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,37 +4,70 @@ import { Button } from "react-bootstrap";
 const Portfolio = () => {
   const projectsInfo = [
     {
-      title: "my-project",
+      title: "Code-Quiz",
       image: "",
-      description: "First project",
-      githubrepolink: "github.com",
-      deploylink: "github.com",
+      description: "Code-Quiz application to test user's knowledge of javascript.User needs to answer the question based on the time given.Timer will deduct 10 secs for selecting every incorrect answer.User should see the final score at the end of the game.View high score button should display the scores.go back button should take the user back to Welcome quiz screen and clear scores should clear all the scores.",
+      githuburl: "https://github.com/ashachakre0906/Code-Quiz",
+      deployurl: "https://ashachakre0906.github.io/Code-Quiz",
     },
     {
-      title: "my-project",
+      title: "Weather-Dashboard",
       image: "",
-      description: "First project",
-      githubrepolink: "github.com",
-      deploylink: "github.com",
+      description: "The challenge is to build a weather dashboard that will run in the browser and feature dynamically updated HTML and CSS.The server-side API used to get response data object is retrieved from the Open Weather API.User should be able to search for any city name which would display current weather conditions,the temperature, the humidity, the wind speed, the UV index and five days forecast of that particular city.We are storing the previously searched cities in Local Storage and persistently displaying the same cities on the webpage.UV index is presented with a color that indicates whether conditions are favorable, moderate, or severe.",
+      githuburl: "https://github.com/ashachakre0906/Weather-Dashboard",
+      deployurl: "https://ashachakre0906.github.io/Weather-Dashboard",
     },
     {
-      title: "my-project",
+      title: "Note-taker",
       image: "",
       description: "First project",
-      githubrepolink: "github.com",
-      deploylink: "github.com",
+      githuburl: "https://github.com/ashachakre0906/note-taker",
+      deployurl: "https://notes-keep-app.herokuapp.com",
     },
     {
-      title: "my-project",
+      title: "Work-Day Planner",
       image: "",
       description: "First project",
-      githubrepolink: "github.com",
-      deploylink: "github.com",
+      githuburl: "https://github.com/ashachakre0906/Work-Day-Planner",
+      deployurl: "https://ashachakre0906.github.io/Work-Day-Planner",
+
+    },
+    {
+      title: "Employee Tracker",
+      image: "",
+      description: "Challenge is to build a command-line application from scratch to manage a company's employee database, using Node.js, Inquirer, and MySQL.These interfaces are called content management systems (CMS) which allows non-developers to easily view and interact with information stored in databases",
+      githuburl: "https://github.com/ashachakre0906/employee-tracker",
+      deployurl: "https://github.com",
+
+    },
+    {
+      title: "Team Profile Generator",
+      image: "",
+      description: "Challenge is to build a Node.js command-line application that takes in information about employees on a software engineering team and generates an HTML webpage that displays that displays a nicely formatted team roster based on user input and summaries for each person. We will also write unit tests for each part of our code and ensure that it passes all of them.",
+      githuburl: "https://github.com/ashachakre0906/team-Profile-Generator",
+      deployurl: "https://ashachakre0906.github.io/team-Profile-Generator",
+
+    },
+    {
+      title: "Project Freakimono",
+      image: "",
+      description: "Welcome to Freakimono! This immersive app combines the joy of creating and caring for a loveable little creature and the fun of connecting with other users to battle it out in a dynamic battle environment. Upon first visit, we'll guide you through the process of setting up a new account and the inception of your new adorable digital friend. Once your new friend is created and named, you be introduced to the playpen where you can adore and care for your new pet's needs. Lucky for you, the messes are virtual and the means to meet your little friends needs are costless! When you are ready to take on the virtual network of other creatures, visit the battle page to duke it out with other users in a head to head memory challenge. Fight for your life and get rewarded.",
+      githuburl: "https://github.com/Team-Narwhal/My_Pet",
+      deployurl: "https://freakimono.herokuapp.com",
+
+    },
+    {
+      title: "Project Pokemon-Battle",
+      image: "",
+      description: "Collaborate with fellow developers to build a front-end application that is interactive (i.e: accept and respond to user input) and utilizes at least two server-side APIs. The application will be live and deployed to Github pages",
+      githuburl: "https://github.com/dnsnguy08/Pokemon-Battle",
+      deployurl: "https://dnsnguy08.github.io/Pokemon-Battle",
+
     },
   ];
   const renderCard = (card, index) => {
     return (
-      <Card style={{ width: "18rem" }} key={index}>
+      <Card style={{ width: "18rem" }} key={index} className='box'>
         <Card.Img variant="top" src={card.image} />
         <Card.Body>
           <Card.Title>{card.title}</Card.Title>
@@ -46,7 +79,7 @@ const Portfolio = () => {
     );
   };
 
-  return <div className="App">
+  return <div className="d-flex justify-content-around">
     {projectsInfo.map(renderCard)}</div>;
 };
 
