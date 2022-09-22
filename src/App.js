42eb4ab3f0a3c04a8121ed67941deb898b9828f2
ashachakre0906 <chourpagar.asha@gmail.com>
2022-09-21 23:21:57 -0700

    started working on component contact form page

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
-import Projects from "./components/Projects";
+// import Projects from "./components/Projects";
 import Resume from "./components/Resume";
 
 import Footer from "./components/Footer";
--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,38 @@
 import React from "react";
 
-
 const Contact = () => {
     return(
-        <div>Contact</div>
+        <>
+        {/* contact us form */}
+        <div>
+            <div className="contact_form">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-10 mx-auto">
+                            <div className="contact_form_container py-5">
+
+
+                            </div>
+
+                        </div>
+
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+        
+        
+        
+        
+        
+        </>
     )
 
 }
