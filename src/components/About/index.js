a99069d8382d3c9d4a1f70eb22b6a6b9b9c197b5
ashachakre0906 <chourpagar.asha@gmail.com>
2022-10-23 00:44:15 -0700

    fixing the portfolio in responsive container

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,14 +5,16 @@ import MainTitle from "./header";
 const About = () => {
   return (
     <div className="about-me">
+      <header>
       <h1 className="hero-heading">Asha Chakre</h1>
       <h2>Full Stack Developer</h2>
+      </header>
       <div>
       <img src={image} alt="Asha Chakre" />
       </div>
       <p>Passionate and Ambitious Full Stack Developer
         and UX/UI enthusiast based in San Jose,CA with the passion of learning and creating apps
-        with prior experience in quality assurance<br></br>
+        with prior experience in quality assurance
         eager to leverage skills and experience 
         in a fast-paced environment and grow as a developer.</p>
     <a href="https://github.com/ashachakre0906" className="btn">Get In Touch</a> 
