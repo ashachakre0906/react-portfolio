8d3645befb2c6738b3eac0fd6d858584a41920d9
ashachakre0906 <chourpagar.asha@gmail.com>
2022-10-31 11:38:17 -0700

    added css properties in resume section

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -7,27 +7,53 @@ import { uploadedFileLink } from "../../assets/images";
 const Resume = () => {
   return (
     <>
-      <a
-        className="resume-sec"
-        href={uploadedFileLink}
-        target="_blank"
-        rel="noopener noreferrer"
-        download
-      >
-      
-        <button className="button">
-          <FontAwesomeIcon icon={uploadedFileLink} />
-          Download Resume
-        </button>
-      </a>
-      <embed className="resume-section"
+      <div className="resume-sec">
+        <h1 className="skill">Skills</h1>
+        <h3 className="skill">Front-end</h3>
+        <div>
+          <ul className="skills-section1">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Javascript</li>
+            <li>Jquery</li>
+            <li>Responsive design</li>
+            <li>React</li>
+            <li>Bootstrap</li>
+          </ul>
+        </div>
+        <h3 className="skill">Back-end</h3>
+        <div>
+          <ul className="skills-section1">
+            <li>APIS</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySql Sequelize</li>
+            <li>MongoDB Mongoose</li>
+            <li>REST</li>
+            <li>GraphQL</li>
+          </ul>
+        </div>
+        {/* <embed className="resume-section"
         src={uploadedFileLink}
         type="application/pdf"
         frameBorder="0"
         scrolling="auto"
         height="100vh"
         width="100%"
-      ></embed>
+       ></embed> */}
+        <a
+          className="button"
+          href={uploadedFileLink}
+          target="_blank"
+          rel="noopener noreferrer"
+          download
+        >
+          <button className="button">
+            <FontAwesomeIcon icon={uploadedFileLink} />
+            Download Resume
+          </button>
+        </a>
+      </div>
     </>
   );
 };
